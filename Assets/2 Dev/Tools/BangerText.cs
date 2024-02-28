0ac545a952a32a3c0e708c5fa7039e53f509a497
Tx Mat <46858765+TxMat@users.noreply.github.com>
2024-02-28 12:53:18 +0100

    MM

---
+++ Assets/2 Dev/Tools/BangerText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class BangerText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        // hue shift
+        text.color = Color.HSVToRGB(Mathf.PingPong(Time.time * .2f, 1), 1, 1);
+    }
+}
